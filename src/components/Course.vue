62b5dc57a82c621251bb58bc6ad0b89351460055
Mohamed akhelij <m.akhelij@gmail.com>
2021-09-20 12:05:34 +0100

    Course component

---
+++ src/components/Course.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-white shadow rounded flex flex-col justify-around items-center w-32 h-36">
+        <span
+            class="mx-auto rounded-full max-h-36 bg-gradient-to-br from-gray-800 to-gray-200 border-2 h-16 w-16"           
+        />
+        <h2 class="text-lg font-semibold">Algébre</h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
